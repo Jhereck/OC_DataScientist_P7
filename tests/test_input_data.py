f174add9f7765d82d7469a9227c3258f038be069
Jhereck <dar55@hotmail.com>
2023-06-14 20:00:02 +0200

    Test tests

--- tests/test_input_data.py
+++ tests/test_input_data.py
@@ -5,7 +5,7 @@ import pandas as pd
 @pytest.fixture(scope="module")
 def data_test():
     """Get customers processed test data to feed into the tests"""
-    return pd.read_csv("./data/processed/test_feature_engineering.csv", index_col=[0])
+    return pd.read_csv("./datas/application_test.csv", index_col=[0])
 
 
 def test_test_duplicates(data_test):
