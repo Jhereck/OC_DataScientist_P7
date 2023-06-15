7b38f9c0d6baeddaa7d30974f6ac9b85d0bedeb2
Jhereck <dar55@hotmail.com>
2023-06-15 12:59:11 +0200

    Api

--- app/api.py
+++ app/api.py
@@ -1,8 +1,32 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+import pandas as pd
+import numpy as np
+import pickle as pk
 
 app = FastAPI()
+
+try:
+    datas = pd.read_csv("./datas/application_test.csv")
+    datas.replace([np.inf, -np.inf], np.nan, inplace=True)
+    datas.fillna(-1, inplace=True)
+except:
+    raise HTTPException(status_code=404, detail="Données non trouvées")
+
+try:
+    model = pk.load(open("./model/model.pkl", "rb"))
+except:
+    raise HTTPException(status_code=404, detail="Modèle non trouvé")
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.post("/predict")
+async def predict(num_client: int):
+    predict = model.predict_proba(datas.iloc[num_client])
+    if predict >= 0.4669:
+        return {"prediction": "Potentiel mauvais payeur"}
+
+    return {"prediction": "Potentiel bon payeur"}
